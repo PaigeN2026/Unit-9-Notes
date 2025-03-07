851d9cfa0ec2c9c89926d61536270eac2d717964
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-03-07 16:06:49 +0000

    3/7

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // Use the default/no-arg constructors 
+      Vehicle v1 = new Vehicle();
+      Car c1 = new Car();
+      // Use the parameterized constructors
+      Vehicle v2 = new Vehicle(3, 10.0, "White");
+      Car c2 = new Car(4, 65.0, "Banana", 5, "Nissan");
+      
+      // 4. En la película Spanglish, la relación de Deb y John no es muy buena. Deb se siente ignorada porque John siempre está concentrado en su trabajo y no le pone atención a ella. Los problemas empeoran porque tienen diferentes formas de hacer sus cosas. Despues de tiempo, Deb se siente frustrada y molesta porque no recibe suficiente atención de John, y ella fue infiel. Si Deb hubiera expresado sus sientimentos antes de llegar a este punto, tal vez a via había una forma de arreglar su situación. Pero yo pienso que la relacion de los dos está muy rota a arreglar.      
+      // 7. En la película Spanglish, la relación entre padres e hijos es un tema muy importante, especialmente entre Flor y su hija, Christina. Flor trabaja duro para cuidar a Christina y darle una vida mejor que la tuvo en México. Christina se siente confundida porque quiere estar con su amigas Americanas, pero también quiero a respetar la cultura a su máma. 
+
    }
 }
